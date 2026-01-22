bd76dfea206b3704fa67026ecce40183edfdedb2
buihongngoct67-arch <buihongngoc_t67@hus.edu.vn>
2026-01-22 14:40:42 +0700

    aft 22/1

--- simple all conditions.py
+++ simple all conditions.py
@@ -16,5 +16,25 @@ gene_counts = uberon_df.groupby('Gene name')['Anatomical entity name'].nunique()
 specific_genes = gene_counts[gene_counts <= 3].index
 
 result_table = uberon_df[uberon_df['Gene name'].isin(specific_genes)]
-final_table = result_table[['Gene name', 'Anatomical entity name', 'Sex', 'Developmental stage name']]
+final_table = result_table[['Gene name', 'Anatomical entity name', 'Sex', 'Developmental stage name', 'Anatomical entity ID']]
 print(final_table)
+
+child_ID = result_table['Anatomical entity ID'].unique()
+import obonet
+obo_path = r"C:\Users\DELL\Documents\GitHub\SRP-Project\uberon.obo"
+uberon_ontology = obonet.read_obo(obo_path)
+
+
+term = "UBERON:0002107"
+term in uberon_ontology
+list(uberon_ontology.keys())[:10]
+
+# parent_id should be parents of "term": "digestive system" and "anatomical system"
+
+parent_ID = [node 
+               for node in uberon_ontology.nodes() 
+               if ("UBERON:0000467", node) in uberon_ontology.edges()
+               and uberon_ontology.edges[("UBERON:0000467", node)].get("relation") == "part_of"]
+
+print(parent_ID)
+                                uberon_ontology[term]
